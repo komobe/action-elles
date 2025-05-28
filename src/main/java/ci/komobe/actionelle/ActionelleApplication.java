845681cf1ebd11dfd56a333eec15a9aee128674f
Moro KONE <komobesokona@gmail.com>
2025-05-28 16:07:06 +0000

    build: ajout du projet Spring Boot initial généré depuis Spring Initializr

---
+++ src/main/java/ci/komobe/actionelle/ActionelleApplication.java
@@ -0,0 +1,13 @@
+package ci.komobe.actionelle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionelleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActionelleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ci/komobe/actionelle/ActionelleApplicationTests.java
@@ -0,0 +1,13 @@
+package ci.komobe.actionelle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActionelleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
